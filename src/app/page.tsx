e8c7040ab1a212033924157d7650beaebb58274b
Randy <36478478+Erelumnir@users.noreply.github.com>
2024-02-16 10:37:13 +0100

    Added Navbar + Button Comps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import SignIn from "@/components/auth/SignIn";
-import Image from "next/image";
+import Navbar from "@/components/interactives/Navbar";
+import Footer from "@/components/statics/Footer";
 
 export default function Home() {
   return (
-    <SignIn />
+    <div>
+      <Navbar />
+      <h1>SiteSculptor</h1>
+      <p>"Empower your web design journey with our intuitive drag-and-drop interface, modular components, seamless e-commerce integration, and robust tools for responsive design, SEO optimization, user management, social media integration, security, and personalized branding."</p>
+      <Footer />
+    </div>
   );
 }
