940130bbbd09abfa5f087cb9282bb2f0231910fa
Randy <36478478+Erelumnir@users.noreply.github.com>
2024-02-16 15:01:11 +0100

    Added Card Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import Navbar from "@/components/interactives/Navbar";
 import Footer from "@/components/statics/Footer";
 import PageHeader from "@/components/statics/PageHeader";
-import "@/resources/styling/pages/index.scss"
 import InfoCards from "@/components/statics/InfoCards"
+import "@/resources/styling/pages/index.scss"
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <PageHeader headerName="SiteSculptor" subHeaderName='"Empower your web design journey with our intuitive drag-and-drop interface, modular components, seamless e-commerce integration, and robust tools for responsive design, SEO optimization, user management, social media integration, security, and personalized branding."' />
+
       <div className="cardRow">
-        <InfoCards text="Some text" type="default" />
+        <InfoCards header="Info Card" text="The tree drank the water from the earth to sustain the growth of it's leaves. And the human took a breath from the oxygen from the tree. So we should thank the trees for helping us breathe." type="info" />
+        <InfoCards header="Info Card" text="The tree drank the water from the earth to sustain the growth of it's leaves. And the human took a breath from the oxygen from the tree. So we should thank the trees for helping us breathe." type="default" />
+        <InfoCards header="Info Card" text="The tree drank the water from the earth to sustain the growth of it's leaves. And the human took a breath from the oxygen from the tree. So we should thank the trees for helping us breathe." type="default" />
+        <InfoCards header="Package 1" text="Really Cool Package" type="pricing" />
       </div>
       <Footer />
     </div>
--- src/components/statics/InfoCards.tsx
+++ src/components/statics/InfoCards.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "@/resources/styling/components/statics/infoCards.scss"
+import DefaultButton from '../interactives/DefaultButton';
 
-type CardTypes = "default" | "imageCard" | "pricing" | "infoLeft" | "infoRight" | "infoTop" | "infoBottom";
+type CardTypes = "default" | "imageCard" | "pricing" | "info";
 
 type cardProps = {
+  header: string;
   text: string;
   type: CardTypes;
 }
@@ -12,17 +14,65 @@ const variants = {
   "default": "defaultCard",
   "imageCard": "imageCard",
   "pricing": "pricingCard",
-  "infoLeft": "infoCard left",
-  "infoRight": "infoCard right",
-  "infoTop": "infoCard top",
-  "infoBottom": "infoCard bottom",
+  "info": "infoCard"
 }
 
-export default function InfoCard({ text, type} : cardProps) {
-  return (
-    <div className={(variants[type ?? "default"])}>
-      
-      {text}
-    </div>
-  )
+function InfoCard({ header, text, type }: cardProps) {
+  let cardContent;
+
+  switch (type) {
+    case "default":
+      cardContent = (
+        <div className={"defaultCard " + (variants[type ?? "default"])}>
+          <h3>{header}</h3>
+          <p>{text}</p>
+        </div>
+      );
+      break;
+    case "imageCard":
+      cardContent = (
+        <div className={"defaultCard " + (variants[type ?? "default"])}>
+          <h3>{header}</h3>
+          <p>{text}</p>
+        </div>
+      );
+      break;
+    case "pricing":
+      cardContent = (
+        <div className={"defaultCard " + (variants[type ?? "default"])}>
+          <div className="packageHeader">
+            <h3>{header}</h3>
+          </div>
+          <p>
+            {text}
+          </p>
+          <div className="packageFooter">
+            <DefaultButton buttonName='Select Package' type='primary' />
+          </div>
+        </div>
+      );
+      break;
+    case "info":
+      cardContent = (
+        <div className={"defaultCard " + (variants[type ?? "default"])}>
+          <h3>{header}</h3>
+          <p>{text}</p>
+          <h3>{header}</h3>
+          <p>{text}</p>
+        </div>
+      );
+      break;
+
+    default:
+      cardContent = (
+        <div className="defaultCard">
+          <h3>{header}</h3>
+          <p>{text}</p>
+        </div>
+      );
+  }
+
+  return cardContent;
 }
+
+export default InfoCard;
