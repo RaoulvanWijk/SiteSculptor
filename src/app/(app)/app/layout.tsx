773f6e59506b6eed4699dbfc0b4524b58204fd80
FinnKuiper <finnkuiper05@gmail.com>
2024-02-28 09:08:00 +0100

    Refactor layout component

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -1,20 +1,6 @@
-import React from 'react'
-import TrpcProvider from "@/lib/trpc/Provider";
-import { cookies } from "next/headers";
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
-export default function layout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <TrpcProvider cookies={cookies().toString()}><div className="flex h-screen">
-            <Sidebar />
-            <main className="flex-1 md:p-8 pt-2 p-8 overflow-y-auto">
-                <Navbar />
-                {children}
-            </main>
-        </div></TrpcProvider>
-    )
+import React from "react";
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+
+export default function layout({ children }: { children: React.ReactNode }) {
+    return <DashboardLayout children={children} />;
 }
