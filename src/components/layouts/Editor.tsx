909a7dfa1b63697ce1015c746d8aeabc7b36e138
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-02-29 11:27:20 +0100

    you can now give classes to layout component

--- src/components/layouts/Editor.tsx
+++ src/components/layouts/Editor.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "@/resources/styling/components/layouts/editor-layout.scss";
+import { cn } from "@/lib/utils";
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="editor-container">{children}</div>;
+export function Layout({ children, className }: { 
+  children: React.ReactNode,
+  className?: string, 
+}) {
+  return <div className={"editor-container " + className}>{children}</div>;
 }
 export function SideNav() {
   return <div className="sidebar border-x">Side nav</div>;
