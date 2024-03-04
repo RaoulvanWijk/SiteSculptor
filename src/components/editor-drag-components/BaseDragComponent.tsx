bb5b35f60ffbe888d5819bcb300ca0329ead61a9
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-03-04 21:10:09 +0100

    slightly changed drag component for easier development

--- src/components/editor-drag-components/BaseDragComponent.tsx
+++ src/components/editor-drag-components/BaseDragComponent.tsx
@@ -2,24 +2,26 @@ import React from 'react'
 // import { useDraggable } from '@dnd-kit/core'
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import {
+  Component,
+  UsedComponent,
+  EditorHandlerState,
+  EditorHandlerProps,
+} from "editor";
 
 export default function BaseDragComponent(
-  { children, component }: {
-    component: {
-      id: string,
-      type: string,
-      name: string,
-    },
+  { children, id }:
+  {
+    id: number | string,
     children?: React.ReactNode,
   }
 ) {
 
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: component.id });
-
-
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: id });
 
   return (
     <div
+    className='w-fit border-2 h-fit'
     ref={setNodeRef}
     {...attributes}
     {...listeners}
@@ -29,7 +31,6 @@ export default function BaseDragComponent(
     }}
     >
       {children}
-      {/* {component.name} */}
     </div>
   )
 }
