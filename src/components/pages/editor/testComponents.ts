643edeac2efe610a63528293e12caf2b5911614d
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-03-04 21:11:40 +0100

    made a file with testcomponents for testing without database

---
+++ src/components/pages/editor/testComponents.ts
@@ -0,0 +1,85 @@
+import { Component, UsedComponent, EditorHandlerState, EditorHandlerProps } from "editor";
+import { nanoid } from "@/lib/utils";
+
+export const testComponents: Component[] = [
+  {
+    id: nanoid(10),
+    name: "Image",
+    type: "image",
+    props: {
+      src: "",
+      alt: "",
+    },
+    styles: {
+      width: "",
+      height: "",
+    },
+    hasChildren: false,
+    allowedChildren: [],
+  },
+  {
+    id: nanoid(10),
+    name: "Text",
+    type: "text",
+    props: {
+      text: "",
+    },
+    styles: {
+      fontSize: "",
+      color: "",
+    },
+    hasChildren: false,
+    allowedChildren: [],
+  },
+  {
+    id: nanoid(10),
+    name: "Button",
+    type: "button",
+    props: {
+      text: "",
+    },
+    styles: {
+      type: "",
+      color: "",
+    },
+    hasChildren: false,
+    allowedChildren: [],
+  },
+  {
+    id: nanoid(10),
+    name: "Container",
+    type: "container",
+    props: {},
+    styles: {},
+    hasChildren: true,
+    allowedChildren: ["text", "button", "image"],
+  },
+];
+
+export const testUsedComponents: UsedComponent[] = [
+  {
+    id: "test-id-container",
+    index: 0,
+    component: testComponents[3],
+    props: {},
+    styles: {},
+    children: [
+      {
+        id: "test-id-text",
+        index: 0,
+        component: testComponents[1],
+        props: { text: "Hello, World!" },
+        styles: { fontSize: "24px", color: "black" },
+        children: [],
+      },
+      {
+        id: "test-id-button",
+        index: 1,
+        component: testComponents[2],
+        props: { text: "Click me!" },
+        styles: { type: "primary", color: "blue" },
+        children: [],
+      },
+    ],
+  },
+];
