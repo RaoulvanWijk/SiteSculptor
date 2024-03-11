f41912c856f95580f5924af0d4ed81a43015c3e3
FinnKuiper <finnkuiper05@gmail.com>
2024-03-11 10:33:47 +0100

    Refactor styling and layout components

--- src/app/(promotion_site)/page.tsx
+++ src/app/(promotion_site)/page.tsx
@@ -1,30 +1,53 @@
 import Navbar from "@/components/interactives/Navbar";
 import Footer from "@/components/statics/Footer";
 import PageHeader from "@/components/statics/PageHeader";
-import InfoCards from "@/components/statics/InfoCards"
-import "@/resources/styling/pages/index.scss"
+import InfoCards from "@/components/statics/InfoCards";
+import "@/resources/styling/pages/index.scss";
+import "@/resources/styling/components/layouts/layout.scss";
+import DefaultButton from "@/components/interactives/DefaultButton";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <PageHeader headerName="SiteSculptor">
-        "Empower your web design journey with our intuitive drag-and-drop interface, modular components, seamless e-commerce integration, and robust tools for responsive design, SEO optimization, user management, social media integration, security, and personalized branding."
-      </PageHeader>
+    return (
+        <div className="layout">
+            <Navbar />
+            <main>
+                <div>
+                    <PageHeader headerName="Unleash your creativity with our powerful visual canvas — no coding required">
+                        Some text that keeps repeating. Some text that keeps
+                        repeating. Some text that keeps repeating. Some text
+                        that keeps repeating. Some text that keeps repeating.
+                        Some text that keeps repeating.
+                    </PageHeader>
+                    <DefaultButton buttonName="Get started" type="primary" />
+                </div>
 
-      <div className="cardRow">
-        <InfoCards header="Info Card" type="info">
-          The tree drank the water from the earth to sustain the growth of it's leaves. And the human took a breath from the oxygen from the tree. So we should thank the trees for helping us breathe.
-        </InfoCards>
-        <InfoCards header="Info Card" type="default">
-          The tree drank the water from the earth to sustain the growth of it's leaves. And the human took a breath from the oxygen from the tree. So we should thank the trees for helping us breathe.
-        </InfoCards>
-        <InfoCards header="Info Card" imgSrc="/placeholders/placeholder.jpg" type="imageCard" />
-        <InfoCards header="Package 1" type="pricing">
-          The tree drank the water from the earth to sustain the growth of it's leaves. And the human took a breath from the oxygen from the tree. So we should thank the trees for helping us breathe.
-        </InfoCards>
-      </div>
-      <Footer />
-    </div>
-  );
+                <div className="cardRow">
+                    <InfoCards header="Info Card" type="info">
+                        The tree drank the water from the earth to sustain the
+                        growth of it's leaves. And the human took a breath from
+                        the oxygen from the tree. So we should thank the trees
+                        for helping us breathe.
+                    </InfoCards>
+                    <InfoCards header="Info Card" type="default">
+                        The tree drank the water from the earth to sustain the
+                        growth of it's leaves. And the human took a breath from
+                        the oxygen from the tree. So we should thank the trees
+                        for helping us breathe.
+                    </InfoCards>
+                    <InfoCards
+                        header="Info Card"
+                        imgSrc="/placeholders/placeholder.jpg"
+                        type="imageCard"
+                    />
+                    <InfoCards header="Package 1" type="pricing">
+                        The tree drank the water from the earth to sustain the
+                        growth of it's leaves. And the human took a breath from
+                        the oxygen from the tree. So we should thank the trees
+                        for helping us breathe.
+                    </InfoCards>
+                </div>
+                <Footer />
+            </main>
+        </div>
+    );
 }
--- src/components/interactives/Navbar.tsx
+++ src/components/interactives/Navbar.tsx
@@ -1,29 +1,29 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import DefaultButton from "@/components/interactives/DefaultButton";
-import "@/resources/styling/components/interactives/navbar.scss"
-import Image from 'next/image'
+import "@/resources/styling/components/interactives/navbar.scss";
+import Image from "next/image";
 
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
-    }
+    };
 
     return (
         <nav>
-            <div className="navBar">
+            <div className="topbar navBar">
                 <Image
                     src={"/branding/logo_temp.png"}
-                    className='logoImage'
+                    className="logoImage"
                     height={75}
                     width={150}
-                    alt='Logo Image'
+                    alt="Logo Image"
                 />
 
-                <div className={`navMenu ${isMenuOpen ? 'is-responsive' : ''}`}>
+                <div className={`navMenu ${isMenuOpen ? "is-responsive" : ""}`}>
                     {/* Hamburger Icon */}
                     <div className="hamburgerIcon" onClick={toggleMenu}>
                         <div></div>
@@ -31,15 +31,31 @@ export default function Navbar() {
                         <div></div>
                     </div>
 
-                    <div className='navLinks'>
-                        <DefaultButton buttonName='Home' type='link' linkTo='' />
-                        <DefaultButton buttonName='FAQ' type='link' linkTo=''/>
-                        <DefaultButton buttonName='Examples' type='link' linkTo=''/>
-                        <DefaultButton buttonName='Pricing' type='link' linkTo=''/>
-                        <DefaultButton buttonName='Login/Register' type='linkPrimary' linkTo=''/>
+                    <div className="navLinks">
+                        <DefaultButton
+                            buttonName="Home"
+                            type="link"
+                            linkTo=""
+                        />
+                        <DefaultButton buttonName="FAQ" type="link" linkTo="" />
+                        <DefaultButton
+                            buttonName="Examples"
+                            type="link"
+                            linkTo=""
+                        />
+                        <DefaultButton
+                            buttonName="Pricing"
+                            type="link"
+                            linkTo=""
+                        />
+                        <DefaultButton
+                            buttonName="Login/Register"
+                            type="linkPrimary"
+                            linkTo=""
+                        />
                     </div>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
