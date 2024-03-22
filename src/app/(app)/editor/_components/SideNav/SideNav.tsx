f193b24c4f63d4cb14e3883b6d76ef7b4d1d68bb
FinnKuiper <finnkuiper05@gmail.com>
2024-03-22 14:18:32 +0100

    Add SideNav component with styling

---
+++ src/app/(app)/editor/_components/SideNav/SideNav.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Button from "./buttons/Button";
+import { Home } from "lucide-react";
+import Navigator from "./BreadCrumps";
+import "@/resources/styling/components/SideNav/sidenav.scss";
+import useSideNav from "@/components/hooks/useSideNav";
+
+export default function SideNav() {
+    const { navType } = useSideNav();
+
+    const navs = [
+        {
+            type: "main",
+            content: [
+                { text: "Home", type: "page-select" },
+                { text: "Contact", type: "page-select" },
+                { text: "Finance", type: "page-select" },
+                { text: "Add new page", type: "page-select" },
+                { text: "Footer", type: "footer" },
+                { text: "Navbar", type: "nav" },
+            ],
+        },
+        {
+            type: "page-select",
+            content: [
+                { text: "Text", type: "page-component" },
+                { text: "Image", type: "page-component" },
+                { text: "Button", type: "page-component" },
+                { text: "Form", type: "page-component" },
+                { text: "List", type: "page-component" },
+                { text: "Table", type: "page-component" },
+            ],
+        },
+    ];
+
+    const currentNav = navs.find((nav) => nav.type === navType);
+
+    if (!currentNav) {
+        return <div className="sidenav">Error</div>;
+    }
+
+    return (
+        <div className="sidenav">
+            <h1 className="sidenav-title">Navigator</h1>
+            <Navigator />
+            <div className="buttons">
+                {currentNav.content.map((button) => (
+                    <Button
+                        text={button.text}
+                        type={button.type}
+                        Icon={<Home />}
+                        key={button.text}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
