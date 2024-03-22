e39688b2790e4405997be32d726605864e901cfe
FinnKuiper <finnkuiper05@gmail.com>
2024-03-22 14:18:57 +0100

    Refactor Editor component layout and add SideNavContextProvider

--- src/components/layouts/Editor.tsx
+++ src/components/layouts/Editor.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import "@/resources/styling/components/layouts/editor-layout.scss";
 import { cn } from "@/lib/utils";
+import { SideNavContextProvider } from "../context/SideNavContext";
 
-export function Layout({ children, className }: { 
-  children: React.ReactNode,
-  className?: string, 
+export function Layout({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
 }) {
-  return <div className={cn("editor-container", className)}>{children}</div>;
+    return <div className={cn("editor-container", className)}>{children}</div>;
 }
 export function SideNav({ children }: { children: React.ReactNode }) {
-  return <div className="sidebar border-x">{children}</div>;
+    return (
+        <div className="sidebar border-x">
+            {" "}
+            <SideNavContextProvider>{children}</SideNavContextProvider>
+        </div>
+    );
 }
 export function TopNav() {
-  return <div className="topnav border-y">EditorTopNav</div>;
+    return <div className="topnav border-y">EditorTopNav</div>;
 }
 
 const Editor = {
-  Layout: Layout,
-  SideNav: SideNav,
-  TopNav: TopNav,
+    Layout: Layout,
+    SideNav: SideNav,
+    TopNav: TopNav,
 };
 
 export default Editor;
