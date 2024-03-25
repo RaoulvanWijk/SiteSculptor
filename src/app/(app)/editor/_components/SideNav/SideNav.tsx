f4d472fe4e37387abca2f9c617af4e601ed4013c
FinnKuiper <finnkuiper05@gmail.com>
2024-03-25 09:35:40 +0100

    Added icons and styling

--- src/app/(app)/editor/_components/SideNav/SideNav.tsx
+++ src/app/(app)/editor/_components/SideNav/SideNav.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import Button from "./buttons/Button";
-import { Home } from "lucide-react";
+import {
+    CaseSensitive,
+    DollarSign,
+    Home,
+    PanelBottom,
+    PanelTop,
+    Phone,
+    Plus,
+    Image,
+    MousePointerClick,
+} from "lucide-react";
 import BreadCrumbs from "./BreadCrumbs";
 import "@/resources/styling/components/SideNav/sidenav.scss";
 import useSideNav from "@/components/hooks/useSideNav";
@@ -14,23 +24,31 @@ export default function SideNav() {
         {
             type: "main",
             content: [
-                { text: "Home", type: "page-select" },
-                { text: "Contact", type: "page-select" },
-                { text: "Finance", type: "page-select" },
-                { text: "Add new page", type: "new-page" },
-                { text: "Footer", type: "footer" },
-                { text: "Navbar", type: "nav" },
+                { text: "Home", type: "page-select", icon: <Home /> },
+                { text: "Contact", type: "page-select", icon: <Phone /> },
+                { text: "Finance", type: "page-select", icon: <DollarSign /> },
+                { text: "Add new page", type: "new-page", icons: <Plus /> },
+                { text: "Footer", type: "footer", icon: <PanelBottom /> },
+                { text: "Navbar", type: "nav", icon: <PanelTop /> },
             ],
         },
         {
             type: "page-select",
             content: [
-                { text: "Text", type: "page-component" },
-                { text: "Image", type: "page-component" },
-                { text: "Button", type: "page-component" },
-                { text: "Form", type: "page-component" },
-                { text: "List", type: "page-component" },
-                { text: "Table", type: "page-component" },
+                {
+                    text: "Text",
+                    type: "page-component",
+                    icon: <CaseSensitive />,
+                },
+                { text: "Image", type: "page-component", icon: <Image /> },
+                {
+                    text: "Button",
+                    type: "page-component",
+                    icon: <MousePointerClick />,
+                },
+                { text: "Form", type: "page-component", icon: <Home /> },
+                { text: "List", type: "page-component", icon: <Home /> },
+                { text: "Table", type: "page-component", icon: <Home /> },
             ],
         },
         {
@@ -64,7 +82,7 @@ export default function SideNav() {
                     <Button
                         text={button.text}
                         type={button.type}
-                        Icon={<Home />}
+                        Icon={button.icon}
                         key={button.text}
                     />
                 ))}
