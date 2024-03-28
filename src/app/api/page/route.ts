456dffbb9934ae985e88717607c8c2e1cb9e88d4
FinnKuiper <finnkuiper05@gmail.com>
2024-03-28 11:01:01 +0100

    Add API routes for site, user, and page creation

--- src/app/api/page/route.ts
+++ src/app/api/page/route.ts
@@ -1,44 +1,9 @@
 import { db } from "@/lib/db/index";
-import { pages } from "@/lib/db/schema/page";
-import { eq } from "drizzle-orm";
+import { pages } from "@/lib/db/schema/pages";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
     const allPages = await db.select().from(pages).execute();
     // show all pages
     return new Response(JSON.stringify(allPages), { status: 200 });
-}
-
-export async function POST(
-    request: Request,
-    slug: string,
-    title: string,
-    user_id: string,
-    page_navbar_id: number,
-    page_footer_id: number
-) {
-    await db.insert(pages).values({
-        id: 0, // Add the id property with a default value
-        slug,
-        title,
-        user_id,
-        page_navbar_id,
-        page_footer_id,
-    });
-    return new Response(JSON.stringify({ message: "ok" }), { status: 200 });
-}
-
-export async function PUT(
-    request: Request,
-    id: number,
-    slug: string,
-    title: string,
-    user_id: string,
-    page_navbar_id: number,
-    page_footer_id: number
-) {
-    await db
-        .update(pages)
-        .set({ id, slug, title, user_id, page_navbar_id, page_footer_id })
-        .where(eq(pages.id, id));
-    return new Response(JSON.stringify({ message: "ok" }), { status: 200 });
 }
