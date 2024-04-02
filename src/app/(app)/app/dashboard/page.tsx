84c201fbbc4d6f5913fa571a4d61d62970598e60
Randy <36478478+Erelumnir@users.noreply.github.com>
2024-04-02 11:48:55 +0200

    Testing GPG key

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     const { session } = await getUserAuth();
     return <main className="space-y-4">
         <h1>Welcome Back</h1>
-        <h3>Continue where you left of</h3>
+        <h3>Continue where you left of..</h3>
         <div className="project-row">
             {/* Logic to import element dynamically */}
             <DashboardCard type="standard" imgSrc="/placeholders/pc.jpg" projectName="Project Name Here" projectDesc="Project description here" />
