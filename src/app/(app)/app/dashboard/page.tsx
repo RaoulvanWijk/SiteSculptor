51c37d605ff50e65c2ea904bfb7cfe83e55d7161
Randy <36478478+Erelumnir@users.noreply.github.com>
2024-04-02 11:20:24 +0200

    test

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
             {/* Logic to import element dynamically */}
             <DashboardCard type="withButton" imgSrc="/placeholders/pc.jpg" projectName="New Extension Released!" projectDesc="Extension description here">
                 <Button type="primary">Check out the Extension!</Button>
-            </DashboardCard>
+            </DashboardCard>git reset --soft HEAD~
         </div>
     </main>;
 }
