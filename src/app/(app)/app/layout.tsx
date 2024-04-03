31d2114e0c051ba03877dab920bf12161f3161ac
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-04-03 09:50:49 +0200

    fixed way of using children for build error

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -2,5 +2,7 @@ import React from "react";
 import DashboardLayout from "@/components/layouts/DashboardLayout";
 
 export default function layout({ children }: { children: React.ReactNode }) {
-    return <DashboardLayout children={children} />;
+    return <DashboardLayout>
+        {children}
+    </DashboardLayout>;
 }
