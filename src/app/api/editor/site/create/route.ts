992b682f9539df22aa8f33d20cd063b910cc1686
FinnKuiper <finnkuiper05@gmail.com>
2024-04-08 12:25:58 +0200

    Add API route for creating a site and implement client-side functionality

--- src/app/api/editor/site/create/route.ts
+++ src/app/api/editor/site/create/route.ts
@@ -1,12 +1,28 @@
 import { db } from "@/lib/db/index";
 import { sites, insertSiteSchema } from "@/lib/db/schema/sites";
 import { pages, insertPageSchema } from "@/lib/db/schema/pages";
+import { getServerSession } from "next-auth";
 import { NextResponse, NextRequest } from "next/server";
+import { getUserAuth } from "@/lib/auth/utils";
 
 export async function POST(request: NextRequest) {
     try {
-        const { ownerId, name } = await request.json();
-        const { error }: any = insertSiteSchema.safeParse({ ownerId, name });
+        const { name } = await request.json();
+        const owner = await getUserAuth();
+        if (!owner) {
+            return new NextResponse(
+                JSON.stringify({ message: "Unauthorized" }),
+                {
+                    status: 401,
+                }
+            );
+        }
+
+        const ownerId = owner.session?.user.id as string;
+
+        console.log(ownerId);
+
+        const { error }: any = insertSiteSchema.safeParse({ name, ownerId });
         if (error) {
             return new NextResponse(
                 JSON.stringify({ message: error.message }),
