a16b089c6f5f95af17679c687b300a81251ca35e
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-04-08 12:51:35 +0200

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = (nextConfig) => {
     return {
-        ...nextConfig,
         images: {
             remotePatterns: [
                 {
@@ -24,14 +23,14 @@ const nextConfig = (nextConfig) => {
                 }
             ],
         },
-        webpack(webpackConfig) {
-            return {
-                ...webpackConfig,
-                optimization: {
-                    minimize: false,
-                },
-            };
-        },
+        // webpack(webpackConfig) {
+        //     return {
+        //         ...webpackConfig,
+        //         optimization: {
+        //             minimize: false,
+        //         },
+        //     };
+        // },
     };
 };
 export default nextConfig;
