656c31d578d0efdd82a1bf79b59bf3950dfb9e2b
FinnKuiper <finnkuiper05@gmail.com>
2024-04-10 09:46:31 +0200

    Refactor file paths and add new API route for getting site data

--- src/app/(app)/[site_id]/[page_id]/editor/page.tsx
+++ src/app/(app)/[site_id]/[page_id]/editor/page.tsx
@@ -13,38 +13,69 @@ import {
     useSensor,
     useSensors,
 } from "@dnd-kit/core";
+import { usePathname, useRouter } from "next/navigation";
 
-const customCollisionDetection = ({
-    droppableContainers,
-    ...args
-  }: any) => {
+const customCollisionDetection = ({ droppableContainers, ...args }: any) => {
     console.log(args.active.data.current, droppableContainers);
-    if(args.active.data.current?.isComponentInEditor) {
-        droppableContainers = droppableContainers.filter((container: any) => container.data.current?.isEditorDroppable !== true);
-
+    if (args.active.data.current?.isComponentInEditor) {
+        droppableContainers = droppableContainers.filter(
+            (container: any) =>
+                container.data.current?.isEditorDroppable !== true
+        );
     } else {
-        droppableContainers = droppableContainers.filter((container: any) => (container.data.current?.isEditorDroppable === true || container.data.current?.isSideNavDropArea === true));
+        droppableContainers = droppableContainers.filter(
+            (container: any) =>
+                container.data.current?.isEditorDroppable === true ||
+                container.data.current?.isSideNavDropArea === true
+        );
     }
-    
+
     // first check if the pointer is over an element
-    const pointerCollission = pointerWithin({...args, droppableContainers});
+    const pointerCollission = pointerWithin({ ...args, droppableContainers });
     console.log(pointerCollission);
-    // TODO: 
+    // TODO:
     if (pointerCollission.length > 0) {
-      return pointerCollission;
+        return pointerCollission;
     }
     // console.log(droppableContainers, args);
-    droppableContainers = droppableContainers.filter((container: any) => (!container.data.current?.isSideNavDropArea));
-    
+    droppableContainers = droppableContainers.filter(
+        (container: any) => !container.data.current?.isSideNavDropArea
+    );
+
     // find the closest element
-    const closestCollision = closestCenter({...args, droppableContainers});
+    const closestCollision = closestCenter({ ...args, droppableContainers });
 
     console.log(closestCollision, "closestCollision");
     return closestCollision;
-
 };
 
-export default function EditorPage() {
+async function getSiteData(site_id: string) {
+    const response = await fetch(`/api/editor/site/${site_id}`);
+    const data = await response.json();
+    return data;
+}
+
+export default async function EditorPage() {
+    const [site, setSite] = useState([]);
+
+    const site_id = usePathname().split("/")[1];
+    console.log(site_id, "site_id");
+
+    useEffect(() => {
+        getSiteData(site_id).then((data) => {
+            console.log(data, "data");
+            setSite(data);
+        });
+    }, []);
+
+    console.log(site, "site");
+
+    // get the route
+
+    // log site data
+
+    // get the site data from the database
+
     const sensors = useSensors(
         useSensor(TouchSensor, {
             activationConstraint: {
@@ -72,7 +103,10 @@ export default function EditorPage() {
         //     </div>
         //   </ScrollArea>
         // </EditorLayout>
-        <DndContext collisionDetection={customCollisionDetection} sensors={sensors}>
+        <DndContext
+            collisionDetection={customCollisionDetection}
+            sensors={sensors}
+        >
             <EditorContextProvider>
                 <EditorHandler />
             </EditorContextProvider>
