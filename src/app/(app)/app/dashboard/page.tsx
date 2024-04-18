2ee73a5465da77a39d38d4c641143ba96af72b38
Randy <36478478+Erelumnir@users.noreply.github.com>
2024-04-18 16:43:43 +0200

    Added check for projects

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -7,6 +7,7 @@ import SkeletonBox from "@/components/dashboard/SkeletonBox";
 import { getUserProjects } from "@/components/dashboard/getUserProjects";
 import Button from "@/components/interactives/Button";
 import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 interface Project {
     id: string,
@@ -15,6 +16,7 @@ interface Project {
 }
 
 export default function Home() {
+    const { data: session, status } = useSession();
     const [loading, setLoading] = useState(true);
     const [projects, setProjects] = useState<Project[] | null>(null);
 
@@ -31,27 +33,33 @@ export default function Home() {
     }, [loading]);
 
     return <main className="space-y-4">
-        <h1>Welcome Back</h1>
-        <h3>Continue where you left of..</h3>
-        <div className="project-row">
-            {/* <SkeletonBox /> */}
-            {projects ? (
-                projects.map((project) => (
-                    <React.Fragment key={project.id}>
-                        <DashboardCard
-                            type="standard"
-                            imgSrc="/placeholders/pc.jpg"
-                            projectName={project.name}
-                            projectDesc={project.description ?? ""}
-                            projectID={project.id}
-                            url={`/editor/${project.id}/0`}
-                        />
-                    </React.Fragment>
-                ))
-            ) : (
-               <SkeletonBox />
-            )}
-        </div>
+        <h1>Welcome back, {session?.user.name}</h1>
+        {projects && projects.length > 0 ? (
+            <>
+                <h3>Continue where you left off...</h3>
+                <div className="project-row">
+                    {projects.map((project) => (
+                        <React.Fragment key={project.id}>
+                            <DashboardCard
+                                type="standard"
+                                imgSrc="/placeholders/pc.jpg"
+                                projectName={project.name}
+                                projectDesc={project.description ?? ""}
+                                projectID={project.id}
+                                url={`/editor/${project.id}/0`}
+                            />
+                        </React.Fragment>
+                    ))}
+                </div>
+            </>
+        ) : (
+            <>
+                <h3>No projects created yet...</h3>
+                <DialogBox title="Create a new Project" description="Give your Project a name" >New Project</DialogBox>
+
+            </>
+        )}
+
         <h3>Latest News</h3>
         <div className="project-row">
             {/* Logic to import element dynamically */}
