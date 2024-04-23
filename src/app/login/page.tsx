a5d2b6fda1bf710be6fe0d31a549174974467796
Lasse Lenting <46818284+lasse-lenting@users.noreply.github.com>
2024-04-23 13:46:22 +0200

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,119 +1,67 @@
-// next auth custom sign in page
-"use client";
-import { Metadata } from "next"
-import Image from "next/image"
-import Link from "next/link"
-import { SessionProvider, useSession } from "next-auth/react"
+// custom nextjs auth login page
+"use client"
+import * as React from "react"
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next"
+import options from "../api/auth/[...nextauth]/route"
+
+import { getProviders, signIn } from "next-auth/react"
+import { useSession } from "next-auth/react";
+
 import { cn } from "@/lib/utils"
-import { useRouter } from 'next/router'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useSearchParams } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
-import { UserAuthForm } from "@/app/login/components/form"
+export function UserAuthForm() {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false)
 
-export default function AuthLayout() {
-  <SessionProvider>
-    <AuthWrapper />
-  </SessionProvider>
-}
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault()
+    setIsLoading(true)
 
-function AuthWrapper() {
-  // if session is active, redirect to callback url in parameter called "callbackUrl"
-  // if session is not active, render login form
-  const { data: session } = useSession()
-  const router = useRouter()
-
-  if (session) {
-    if (router.query.callbackUrl) {
-      router.push(`/callback?callbackUrl=${router.query.callbackUrl}`)
-    
-    } else {
-      router.push(`/app/dashboard`)
-    }
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
   }
-
+  const searchParams = useSearchParams()
+ 
+  const search = searchParams.get('callbackUrl')
+  // if user is logged in, redirect to the callback url
+  const { status } = useSession();
+  if(status === "authenticated") {
+    redirect(search || "/")
+  }
   return (
-    <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/examples/authentication"
-          className="absolute right-4 top-4 md:right-8 md:top-8"
-        >
-          Login
-        </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            SiteSculptor
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
+    <div className={cn("grid gap-6")}>
+        <div className="grid gap-2">
+          <Button disabled={isLoading} onClick={()=>signIn(
+            // get callback url from params
+            'google', { callbackUrl: search || ''}
+          )}>
+            {isLoading && (
+              <p></p>
+            )}
+            Sign in with Google
+          </Button>
         </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Login to your account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Welcome back! Sign in to your account to continue.
-              </p>
-            </div>
-            <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
-          </div>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
         </div>
       </div>
-    </>
+      
+    </div>
   )
 }
+
+export default UserAuthForm
