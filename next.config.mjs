3b9d82d1d46e8e898d161a2212edbc1c24588f03
FinnKuiper <finnkuiper05@gmail.com>
2024-04-24 10:17:41 +0200

    Add security headers to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -20,8 +20,41 @@ const nextConfig = {
                 hostname: "placehold.co",
                 port: "",
                 pathname: "/**",
-            }
+            },
         ],
+    },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "X-XSS-Protection",
+                        value: "1; mode=block",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "same-origin",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://www.google.com https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self'; media-src 'self'; frame-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; manifest-src 'self'; worker-src 'self'; frame-ancestors 'none';",
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=31536000; includeSubDomains; preload",
+                    },
+                ],
+            },
+        ];
     },
     productionBrowserSourceMaps: true,
 };
