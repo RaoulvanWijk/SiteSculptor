cc58c447e11e8547d4835b1796f8ca562897fb55
FinnKuiper <finnkuiper05@gmail.com>
2024-04-25 13:50:27 +0200

    Add new siteFooters and siteNavbars API queries and mutations

---
+++ src/lib/db/schema/siteFooters.ts
@@ -0,0 +1,50 @@
+import { sql } from "drizzle-orm";
+import { varchar, timestamp, mysqlTable } from "drizzle-orm/mysql-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+import { sites } from "./sites"
+import { type getSiteFooters } from "@/lib/api/siteFooters/queries";
+
+import { nanoid, timestamps } from "@/lib/utils";
+
+
+export const siteFooters = mysqlTable('site_footers', {
+  id: varchar("id", { length: 191 }).primaryKey().$defaultFn(() => nanoid()),
+  siteId: varchar("site_id", { length: 256 }).references(() => sites.id, { onDelete: "cascade" }).notNull(),
+  
+  createdAt: timestamp("created_at")
+    .notNull()
+    .default(sql`now()`),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .default(sql`now()`),
+
+});
+
+
+// Schema for siteFooters - used to validate API requests
+const baseSchema = createSelectSchema(siteFooters).omit(timestamps)
+
+export const insertSiteFooterSchema = createInsertSchema(siteFooters).omit(timestamps);
+export const insertSiteFooterParams = baseSchema.extend({
+  siteId: z.coerce.string().min(1)
+}).omit({ 
+  id: true
+});
+
+export const updateSiteFooterSchema = baseSchema;
+export const updateSiteFooterParams = baseSchema.extend({
+  siteId: z.coerce.string().min(1)
+})
+export const siteFooterIdSchema = baseSchema.pick({ id: true });
+
+// Types for siteFooters - used to type API request params and within Components
+export type SiteFooter = typeof siteFooters.$inferSelect;
+export type NewSiteFooter = z.infer<typeof insertSiteFooterSchema>;
+export type NewSiteFooterParams = z.infer<typeof insertSiteFooterParams>;
+export type UpdateSiteFooterParams = z.infer<typeof updateSiteFooterParams>;
+export type SiteFooterId = z.infer<typeof siteFooterIdSchema>["id"];
+    
+// this type infers the return from getSiteFooters() - meaning it will include any joins
+export type CompleteSiteFooter = Awaited<ReturnType<typeof getSiteFooters>>["siteFooters"][number];
+
---
+++ src/lib/db/schema/siteNavbars.ts
@@ -0,0 +1,50 @@
+import { sql } from "drizzle-orm";
+import { varchar, timestamp, mysqlTable } from "drizzle-orm/mysql-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+import { sites } from "./sites"
+import { type getSiteNavbars } from "@/lib/api/siteNavbars/queries";
+
+import { nanoid, timestamps } from "@/lib/utils";
+
+
+export const siteNavbars = mysqlTable('site_navbars', {
+  id: varchar("id", { length: 191 }).primaryKey().$defaultFn(() => nanoid()),
+  siteId: varchar("site_id", { length: 256 }).references(() => sites.id, { onDelete: "cascade" }).notNull(),
+  
+  createdAt: timestamp("created_at")
+    .notNull()
+    .default(sql`now()`),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .default(sql`now()`),
+
+});
+
+
+// Schema for siteNavbars - used to validate API requests
+const baseSchema = createSelectSchema(siteNavbars).omit(timestamps)
+
+export const insertSiteNavbarSchema = createInsertSchema(siteNavbars).omit(timestamps);
+export const insertSiteNavbarParams = baseSchema.extend({
+  siteId: z.coerce.string().min(1)
+}).omit({ 
+  id: true
+});
+
+export const updateSiteNavbarSchema = baseSchema;
+export const updateSiteNavbarParams = baseSchema.extend({
+  siteId: z.coerce.string().min(1)
+})
+export const siteNavbarIdSchema = baseSchema.pick({ id: true });
+
+// Types for siteNavbars - used to type API request params and within Components
+export type SiteNavbar = typeof siteNavbars.$inferSelect;
+export type NewSiteNavbar = z.infer<typeof insertSiteNavbarSchema>;
+export type NewSiteNavbarParams = z.infer<typeof insertSiteNavbarParams>;
+export type UpdateSiteNavbarParams = z.infer<typeof updateSiteNavbarParams>;
+export type SiteNavbarId = z.infer<typeof siteNavbarIdSchema>["id"];
+    
+// this type infers the return from getSiteNavbars() - meaning it will include any joins
+export type CompleteSiteNavbar = Awaited<ReturnType<typeof getSiteNavbars>>["siteNavbars"][number];
+
