868728be31d6e37bf374e51e0075d51108b8737c
Lasse Lenting <46818284+lasse-lenting@users.noreply.github.com>
2024-05-08 10:46:31 +0200

    pusj

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 // custom nextjs auth login page
 "use client"
 import { useState } from "react"
-
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 import { signIn } from "next-auth/react"
 import { useSession } from "next-auth/react";
-
+import Image from "next/image";
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { useSearchParams } from 'next/navigation'
@@ -30,29 +33,55 @@ export default function Login() {
     redirect(search || "/app/dashboard")
   }
   return (
-    <div className={cn("grid gap-6")}>
-        <div className="grid gap-2">
-          <Button disabled={isLoading} onClick={()=>signIn(
+    <div className="h-screen">
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] h-full">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Select a provider to login 
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <Button variant="outline" className="w-full" onClick={()=>signIn(
             // get callback url from params
             'google', { callbackUrl: search || '/app/dashboard'}
           )}>
             {isLoading && (
               <p></p>
             )}
-            Sign in with Google
+            Login with Google
           </Button>
-        </div>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
+          {/* github */}
+          <Button variant="outline" className="w-full" onClick={()=>signIn(
+            // get callback url from params
+            'github', { callbackUrl: search || '/app/dashboard'}
+          )}>
+            {isLoading && (
+              <p></p>
+            )}
+            Login with Github
+          </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" className="underline">
+              Sign up
+            </Link>
+          </div>
         </div>
       </div>
-      
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
     </div>
   )
 }
