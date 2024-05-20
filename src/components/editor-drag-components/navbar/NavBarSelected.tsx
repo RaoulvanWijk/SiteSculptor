e4693a8a71499e2ef2da87a076b26a736f8aa2b5
FinnKuiper <finnkuiper05@gmail.com>
2024-05-20 19:02:03 +0200

    Added responsiveness to the selectable navs

--- src/components/editor-drag-components/navbar/NavBarSelected.tsx
+++ src/components/editor-drag-components/navbar/NavBarSelected.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "@/resources/styling/components/SideNav/navbarselected.scss";
 import useSideNav from "@/components/hooks/useSideNav";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function NavBarSelected({ className, stijl, sideNav }: any) {
     const { page } = useSideNav();
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     if (sideNav) {
         return (
@@ -19,8 +28,20 @@ export default function NavBarSelected({ className, stijl, sideNav }: any) {
     } else {
         return (
             <nav className={`navbarselected ${className}`} style={stijl}>
-                <div>LOGO</div>
-                <menu className="navbarselected-items">
+                <div className={"hamburger-top"}>
+                    <Link href={"/"}>LOGO</Link>
+                    {/* Hamburger Icon */}
+                    <div className="hamburgerIcon" onClick={toggleMenu}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+                <menu
+                    className={`navbarselected-items ${
+                        isMenuOpen ? "open" : ""
+                    }`}
+                >
                     {page.map((page) => (
                         <li key={page.id}>
                             <a href={page.id}>{page.title}</a>
