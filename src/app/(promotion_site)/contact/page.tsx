0843dbbc8b08a17236f104bd815d526bec18255f
FinnKuiper <finnkuiper05@gmail.com>
2024-05-23 13:02:54 +0200

    Refactor FAQ page and landing page components

--- src/app/(promotion_site)/contact/page.tsx
+++ src/app/(promotion_site)/contact/page.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import "@/resources/styling/pages/contact.scss";
 import Image from "next/image";
 import Head from "next/head";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Avklo | Contact",
+    keywords:
+        "Websites, web builder, website builder, Avklo, avklo, avklo website builder, website builder, easy website builder, fast website builder, website builder for free, website builder for business, website builder for personal use,",
+    description:
+        "Get in touch with us. Do you have probles or questions? Feel free to contact us in the form below. Or check our email and other socials where you can reach us.",
+};
 
 export default function page() {
     return (
         <>
-            <Head>
-                <meta
-                    name="keywords"
-                    content="Websites, web builder, website builder, Avklo, avklo, avklo website builder, website builder, easy website builder, fast website builder, website builder for free, website builder for business, website builder for personal use,"
-                />
-                <meta
-                    name="description"
-                    content="Get in touch with us. Do you have probles or questions? Feel free to contact us in the form below. Or check our email and other socials where you can reach us."
-                    key="desc"
-                />
-            </Head>
             <div className="page-header-contact">
                 <div className="left">
                     <h1>Contact</h1>
