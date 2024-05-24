c1d13c926d39efb7564237b6b5efa93c4472e04f
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-05-24 17:36:01 +0200

    added some better styling for the editor

--- src/components/editor-drag-components/BaseDragComponent.tsx
+++ src/components/editor-drag-components/BaseDragComponent.tsx
@@ -2,23 +2,28 @@ import React from "react";
 // import { useDraggable } from '@dnd-kit/core'
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS, Transform } from "@dnd-kit/utilities";
+import "@/resources/styling/components/editor/components.scss";
+
 import {
   Component,
   UsedComponent,
   EditorHandlerState,
   EditorHandlerProps,
 } from "editor";
+import { cn } from "@/lib/utils";
 
 export default function BaseDragComponent({
   children,
   id,
   data,
   disabled,
+  type,
 }: {
   id: number | string;
   children?: React.ReactNode;
   data?: any;
   disabled?: boolean;
+  type?: "container" | "item";
 }) {
   const {
     attributes,
@@ -47,8 +52,12 @@ export default function BaseDragComponent({
         transform: CSS.Transform.toString(transform as Transform),
         transition: transition,
       }}
-      className={isOver ? "bg-gray-200" : "bg-white"}
-      // className="h-fit"
+      className={
+        cn(
+          isOver ? "bg-gray-200" : "bg-white",
+          type ? (type === "container" ? "base-container" : "base-item") : "base-item"
+        )
+      }
     >
       {children}
     </div>
