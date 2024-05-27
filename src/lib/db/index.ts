d4870664bb84a8c6504d14774455aa0a0141a3b8
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-05-27 20:23:05 +0200

    added all schemas to schema object

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,6 +4,13 @@ import { env } from "@/lib/env.mjs";
 
 import * as pageComponents from "./schema/pageComponents";
 import * as pages from "./schema/pages";
+import * as sites from "./schema/sites";
+import * as components from "./schema/components";
+import * as componentTypes from "./schema/componentTypes";
+import * as footers from "./schema/footers";
+import * as navbars from "./schema/navbars";
+import * as siteFooters from "./schema/siteFooters";
+import * as siteNavbars from "./schema/siteNavbars";
  
 export const poolConnection = mysql.createPool(env.DATABASE_URL);
  
@@ -12,6 +19,13 @@ export const db = drizzle(poolConnection,
         schema: {
             ...pageComponents,
             ...pages,
+            ...sites,
+            ...components,
+            ...componentTypes,
+            ...footers,
+            ...navbars,
+            ...siteFooters,
+            ...siteNavbars,
             // Add your tables here
         },
         mode: "default",
