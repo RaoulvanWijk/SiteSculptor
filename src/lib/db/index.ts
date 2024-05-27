f38771ef2b75084c7223dde1e0d79297a38defed
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-05-27 20:18:38 +0200

    added 2 schemas to schema object in config for relation

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,19 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import { env } from "@/lib/env.mjs";
+
+import * as pageComponents from "./schema/pageComponents";
+import * as pages from "./schema/pages";
  
 export const poolConnection = mysql.createPool(env.DATABASE_URL);
  
-export const db = drizzle(poolConnection);
+export const db = drizzle(poolConnection,
+    {
+        schema: {
+            ...pageComponents,
+            ...pages,
+            // Add your tables here
+        },
+        mode: "default",
+    }
+);
