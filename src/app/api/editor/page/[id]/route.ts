080d5d1a598f19339cb8d7389c6f57dbd2b23025
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-05-28 09:59:47 +0200

    now returning the components with the page

--- src/app/api/editor/page/[id]/route.ts
+++ src/app/api/editor/page/[id]/route.ts
@@ -7,15 +7,60 @@ export async function GET(request: NextRequest, { params }: any) {
     try {
         const id: any = params.id;
 
-        const idPages = await db
-            .select()
-            .from(pages)
-            .where(eq(pages.id, id))
-            .execute();
-        return new NextResponse(JSON.stringify(idPages), {
+        // const idPages = await db
+        //     .select()
+        //     .from(pages)
+        //     .where(eq(pages.id, id))
+        //     .execute();
+
+        let page = await db.query.pages.findMany({
+            with: {
+                pageComponents: {
+                    with: {
+                        children: {
+                            with: {
+                                component: {
+                                    with: {
+                                        type: true,
+                                    },
+                                },
+                            },
+                        },
+                        component: {
+                            with: {
+                                type: true,
+                            },
+                        },
+                    },
+                    where: (pageComponents, { isNull }) => isNull(pageComponents.parentId),
+                },
+            },
+            where: (pages, { eq }) => {
+                return eq(pages.id, id);
+            },
+        });
+        // TODO - Remove this code if chidren is already an empty array
+        // page[0].pageComponents.map((pageComponent: any) => {
+        //     pageComponent.component.type = pageComponent.component.type.name;
+        //     if (pageComponent.children.length > 0) {
+        //       pageComponent.children.map((child: any) => {
+        //         child.component.type = child.component.type.name;
+        //       });
+        //     } else {
+        //       pageComponent.children = [];
+        //     }
+        //   });
+        if (page.length === 0) {
+            return new NextResponse(JSON.stringify({ message: "No pages found" }), {
+                status: 404,
+            });
+        }
+        return new NextResponse(JSON.stringify(page[0]), {
             status: 200,
         });
     } catch (error) {
+        console.log(error);
+        
         return new NextResponse(JSON.stringify({ message: "Invalid JSON" }), {
             status: 400,
         });
