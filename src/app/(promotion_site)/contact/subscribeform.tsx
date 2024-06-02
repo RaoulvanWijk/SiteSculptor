88ff7a6cfc1699336f39ebded502621a916de5d4
ayub <robleajub@Gmail.com>
2024-06-02 22:19:26 +0200

    form werkend

--- src/app/(promotion_site)/contact/subscribeform.tsx
+++ src/app/(promotion_site)/contact/subscribeform.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 const ContactForm: React.FC = () => {
   return (
-    <form action="https://formspree.io/f/mpzvgdkb" method="POST">
-      <label>
-        Your email:
-        <input type="email" name="email" required />
-      </label>
-      <label>
-        Your message:
-        <textarea name="message" required></textarea>
-      </label>
-      <button type="submit">Send</button>
+    <form id="fs-frm" name="simple-contact-form" acceptCharset="utf-8" action="https://formspree.io/f/mpzvgdkb" method="post">
+      <fieldset id="fs-frm-inputs">
+        <label htmlFor="full-name">Full Name</label>
+        <input type="text" name="name" id="full-name" placeholder="First and Last" required />
+        <label htmlFor="email-address">Email Address</label>
+        <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required />
+        <label htmlFor="message">Message</label>
+        <textarea rows={5} name="message" id="message" placeholder=" Your Message ..." required></textarea>
+        <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission" />
+      </fieldset>
+      <input type="submit" value="Submit" />
     </form>
   );
 };
