b8c9fb3e4e7b4e674cf7e3b0a4d22963c2d62480
ayub <robleajub@Gmail.com>
2024-06-03 09:27:48 +0200

    form naar react ipv html

--- next.config.mjs
+++ next.config.mjs
@@ -46,7 +46,7 @@ const nextConfig = {
                     },
                     {
                         key: "Content-Security-Policy",
-                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' https://authjs.dev/img/providers/ data:; font-src 'self' data:; connect-src 'self'; media-src 'self'; frame-src 'self'; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; manifest-src 'self'; worker-src 'self';",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' https://authjs.dev/img/providers/ data:; font-src 'self' data:; connect-src 'self' https://formspree.io/f/mpzvgdkb; media-src 'self'; frame-src 'self'; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; manifest-src 'self'; worker-src 'self';",
                     },
                     {
                         key: "Strict-Transport-Security",
--- src/app/(promotion_site)/contact/subscribeform.tsx
+++ src/app/(promotion_site)/contact/subscribeform.tsx
@@ -1,20 +1,39 @@
+"use client";
 import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
-const ContactForm: React.FC = () => {
-  return (
-    <form id="fs-frm" name="simple-contact-form" acceptCharset="utf-8" action="https://formspree.io/f/mpzvgdkb" method="post">
-      <fieldset id="fs-frm-inputs">
-        <label htmlFor="full-name">Full Name</label>
-        <input type="text" name="name" id="full-name" placeholder="First and Last" required />
-        <label htmlFor="email-address">Email Address</label>
-        <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required />
-        <label htmlFor="message">Message</label>
-        <textarea rows={5} name="message" id="message" placeholder=" Your Message ..." required></textarea>
-        <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission" />
-      </fieldset>
-      <input type="submit" value="Submit" />
-    </form>
-  );
-};
-
-export default ContactForm;
+export default function ContactForm() {
+    const [state, handleSubmit] = useForm("mpzvgdkb");
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">
+                Email Address
+            </label>
+            <input
+                id="email"
+                type="email"
+                name="email"
+            />
+            <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+            />
+            <textarea
+                id="message"
+                name="message"
+            />
+            <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+            />
+            <button type="submit" disabled={state.submitting}>
+                Submit
+            </button>
+        </form>
+    );
+}
