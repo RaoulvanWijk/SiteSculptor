4dc2a9159eccd303e7ca6e53653bf3840d01934d
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-07-01 17:10:41 +0200

    added toaster context

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { checkAuth } from '@/lib/auth/utils'
+import React from "react";
+import { checkAuth } from "@/lib/auth/utils";
+import { Toaster } from "@/components/ui/toaster";
 export default async function layout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    await checkAuth();
-    return (
-        <>{children}</>
-    )
+  await checkAuth();
+  return (
+    <>
+      {children}
+      <Toaster />
+    </>
+  );
 }
